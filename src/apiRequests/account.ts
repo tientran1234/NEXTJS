f372ff4d769a6123ea60b7665933da34161ba7e7
tientran1234 <tranngoctien29112003@gmail.com>
2024-09-06 16:53:21 +0700

    edit error body

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -1,19 +1,19 @@
 import http from "@/lib/http";
 import queryString from "query-string"
 import { AccountListResType, AccountResType, ChangePasswordBodyType, CreateEmployeeAccountBodyType, CreateGuestBodyType, CreateGuestResType, GetGuestListQueryParamsType, GetListGuestsResType, UpdateEmployeeAccountBodyType, UpdateMeBodyType } from "@/schemaValidations/account.schema";
-const prefix= '/accounts'
-export const accountApiRequest ={
+const prefix = '/accounts'
+export const accountApiRequest = {
     me: () => http.get<AccountResType>('accounts/me'),
-    updateMe:(body:UpdateMeBodyType)=> http.put<AccountResType>(`${prefix}/me`,body),
-    changePassword:(body:ChangePasswordBodyType)=> http.put<AccountResType>(`${prefix}/change-password`,body),
-    list:()=>http.get<AccountListResType>(`${prefix}`),
-    addEmployee:(body:CreateEmployeeAccountBodyType)=>http.post<AccountResType>(prefix,body),
-    updateEmployee:(id:number,body:UpdateEmployeeAccountBodyType) => http.put<AccountResType>(`${prefix}/detail/${id}`,body),
-    getEmployee:(id:number)=>http.get<AccountResType>(`${prefix}/detail/${id}`),
-    deleteEmployee:(id:number)=> http.delete<AccountResType>(`${prefix}/detail/${id}`),
-    guestList:(queryParams:GetGuestListQueryParamsType) =>http.get<GetListGuestsResType>(`${prefix}/guests?` + queryString.stringify({
-        fromDate:queryParams.fromDate?.toISOString(),
-        toDate:queryParams.toDate?.toISOString()
+    updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>(`${prefix}/me`, body),
+    changePassword: (body: ChangePasswordBodyType) => http.put<AccountResType>(`${prefix}/change-password`, body),
+    list: () => http.get<AccountListResType>(`${prefix}`),
+    addEmployee: (body: CreateEmployeeAccountBodyType) => http.post<AccountResType>(prefix, body),
+    updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) => http.put<AccountResType>(`${prefix}/detail/${id}`, body),
+    getEmployee: (id: number) => http.get<AccountResType>(`${prefix}/detail/${id}`),
+    deleteEmployee: (id: number, body: { id: number }) => http.delete<AccountResType>(`${prefix}/detail/${id}`, body),
+    guestList: (queryParams: GetGuestListQueryParamsType) => http.get<GetListGuestsResType>(`${prefix}/guests?` + queryString.stringify({
+        fromDate: queryParams.fromDate?.toISOString(),
+        toDate: queryParams.toDate?.toISOString()
     })),
-    createGuest:(body:CreateGuestBodyType) => http.post<CreateGuestResType>(`${prefix}/guests?`,body)
+    createGuest: (body: CreateGuestBodyType) => http.post<CreateGuestResType>(`${prefix}/guests?`, body)
 }
